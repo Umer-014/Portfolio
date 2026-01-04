1f35078849e9df03a2299739df8f3a72dba53b68
Umer-014 <umer.riaz2810@gmail.com>
2026-01-04 19:51:01 +0500

    update the header section.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,7 @@
-import React,{ useState } from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import logo from "../assets/logo.png";
 import { Link } from "react-router-dom";
-
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -10,33 +9,40 @@ const Header = () => {
   return (
     <header className="header">
       <div className="header-container">
-
         {/* Left side: Logo image + Full Name */}
-        <div className="brand">
+        <Link to="/about" className="brand" onClick={() => setMenuOpen(false)}>
           <img src={logo} alt="Muhammad Umer Riaz Logo" className="logo-img" />
           <span className="name">Muhammad Umer Riaz</span>
-        </div>
+        </Link>
 
         {/* Navigation */}
         <nav className={`nav ${menuOpen ? "open" : ""}`}>
-          <Link to="/" onClick={() => setMenuOpen(false)}>Home</Link>
-          <Link to="/about" onClick={() => setMenuOpen(false)}>About</Link>
-          <Link to="/skills" onClick={() => setMenuOpen(false)}>Skills</Link>
-          <Link to="/projects" onClick={() => setMenuOpen(false)}>Projects</Link>
-          <Link to="/cv" onClick={() => setMenuOpen(false)}>CV</Link>
-          <Link to="/contact" onClick={() => setMenuOpen(false)}>Contact</Link>
+          <Link to="/" onClick={() => setMenuOpen(false)}>
+            Home
+          </Link>
+          <Link to="/about" onClick={() => setMenuOpen(false)}>
+            About
+          </Link>
+          <Link to="/skills" onClick={() => setMenuOpen(false)}>
+            Skills
+          </Link>
+          <Link to="/projects" onClick={() => setMenuOpen(false)}>
+            Projects
+          </Link>
+          <Link to="/cv" onClick={() => setMenuOpen(false)}>
+            CV
+          </Link>
+          <Link to="/contact" onClick={() => setMenuOpen(false)}>
+            Contact
+          </Link>
         </nav>
 
         {/* Mobile Menu */}
-        <div
-          className="menu-toggle"
-          onClick={() => setMenuOpen(!menuOpen)}
-        >
+        <div className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
           <span></span>
           <span></span>
           <span></span>
         </div>
-
       </div>
     </header>
   );
